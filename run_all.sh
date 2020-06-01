4fdd17db32196aab9f687e1a932f146befd7aeed
Joel Day <joday@microsoft.com>
2020-06-01 15:25:54 +0000

    Fixed bug in run_all.sh and fixed issue with duplicate hostnames

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,10 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+cd $SCRIPTPATH
+
 echo "Make sure you execute 'export AZURE_SUBSCRIPTION_ID=<SUB ID>' prior to running this script"
-ansible-playbook -i $SCRIPTPATH/hosts.yml $SCRIPTPATH/all.yml
-ansible-playbook -i $SCRIPTPATH/cp-ansible/hosts.yml $SCRIPTPATH/cp-ansible/all.yml
+
+ansible-playbook -i hosts.yml all.yml
+
+cd cp-ansible
+ansible-playbook -i hosts.yml all.yml
